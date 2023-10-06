44578e93428123e71beaa5367a74906649ebb0ee
colddsam <dassamratkumar772@gmail.com>
2023-10-06 21:01:32 +0530

    commit

--- app.js
+++ app.js
@@ -76,4 +76,13 @@ function sendMail() {
                 .catch();
 }
 
+// "https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js">
+// "https://cdnjs.cloudflare.com/ajax/libs/jquery.ripples/0.5.3/jquery.ripples.min.js">
+        $(".body").ripples({
+            resolution: 1024,
+            dropRadius: 20,
+            interactive: true,
+            perturbance: 0.02,
+        });
+
 main()
